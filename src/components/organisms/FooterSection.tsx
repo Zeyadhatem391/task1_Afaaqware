9eb391b823a81169c82f6175fac68d310ca056bb
Zeyadhatem391 <zeyadhatemsabry@gmail.com>
2026-03-29 00:05:36 +0200

    add FooterSection

---
+++ src/components/organisms/FooterSection.tsx
@@ -0,0 +1,7 @@
+export default function FooterSection() {
+  return (
+    <div className="bg-blue-400 text-white w-full p-5 flex justify-center items-center">
+      FooterSection
+    </div>
+  );
+}
